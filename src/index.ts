fe6d3f2d268849e01bcb0424b77550ab15ad5891
ChenX <maple6@vip.qq.com>
2017-07-04 16:20:59 +0800

    fix bug. this.call error

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,32 @@
 let aopMap = new Map<any, AopData>();
 export function iaop()
 {
-    return function (target: Object, propertyKey: string, descriptor: PropertyDescriptor)
+    return function (target: Object, propertyKey: string, descriptor?: PropertyDescriptor)
     {
-        let data = new AopData(target, propertyKey, descriptor);
-        aopMap.set(data.exec, data);
+        if (!descriptor)
+            descriptor = target[propertyKey];
+        let data = new AopData();
+        let methon = descriptor.value;
+        descriptor.value = function (...args)
+        {
+            data.m_Begin.forEach(f =>
+            {
+                f();
+            })
+            methon.apply(this, args);
+            data.m_Ending.forEach(f =>
+            {
+                f();
+            })
+        }
+        aopMap.set(descriptor.value, data);
     };
 }
 class AopData
 {
-    m_Method: any;
-
-    m_Begin = [];
+    m_Begin = []
     m_Ending = []
-    m_Target
-    constructor(target: Object, propertyKey: string, descriptor: PropertyDescriptor)
-    {
-        this.m_Method = descriptor.value;
-        descriptor.value = this.exec;
-        this.m_Target = target
-    }
-    exec = (...args) =>
-    {
-        this.m_Begin.forEach(f =>
-        {
-            f.apply(this.m_Target, args);
-        })
-        let res = this.m_Method.apply(this.m_Target, args);
-        args.push(res);
-        this.m_Ending.forEach(f =>
-        {
-            f.apply(this.m_Target, args);
-        })
-        return res;
-    }
 }
-
 export namespace xaop
 {
     export function begin(f: any, call)
