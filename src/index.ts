703bfbe3f7e52ed32d37990bafa8dd81da2a3f4b
ChenX <maple6@vip.qq.com>
2017-07-05 09:38:58 +0800

    支持箭头函数

--- src/index.ts
+++ src/index.ts
@@ -1,32 +1,51 @@
 let aopMap = new Map<any, AopData>();
-export function iaop()
+
+export function iaop(target: Object, propertyKey: string | symbol, descriptor?)
 {
-    return function (target: Object, propertyKey: string, descriptor?: PropertyDescriptor)
+    let data = new AopData();
+    let _oldFunc;
+    let newMethon = function (...args)
     {
-        if (!descriptor)
-            descriptor = target[propertyKey];
-        let data = new AopData();
-        let methon = descriptor.value;
-        descriptor.value = function (...args)
+        data.m_Begin.forEach(f =>
         {
-            data.m_Begin.forEach(f =>
-            {
-                f();
-            })
-            methon.apply(this, args);
-            data.m_Ending.forEach(f =>
-            {
-                f();
-            })
-        }
-        aopMap.set(descriptor.value, data);
-    };
-}
+            f();
+        })
+        _oldFunc(...args);
+        data.m_Ending.forEach(f =>
+        {
+            f();
+        })
+    }
+    if (!descriptor)
+    {
+        var getter = function ()
+        {
+            return newMethon;
+        };
+        var setter = function (newVal)
+        {
+            _oldFunc = newVal;
+        };
+        Object.defineProperty(target, propertyKey, {
+            get: getter,
+            set: setter,
+            enumerable: true,
+            configurable: true
+        })
+    }
+    else
+    {
+        _oldFunc = descriptor.value;
+        descriptor.value = newMethon
+    }
+    aopMap.set(newMethon, data);
+};
 class AopData
 {
     m_Begin = []
     m_Ending = []
 }
+
 export namespace xaop
 {
     export function begin(f: any, call)
